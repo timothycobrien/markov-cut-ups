63eb492cc49bc5054959e4507cb8a3d74e8798ca
timothycobrien <timothy@obrienlighting.com>
2018-12-11 22:14:29 -0600

    Did algorithm for creation of dictionary, besides possible filename issues which will be fixed

--- src/Dictionary.cpp
+++ src/Dictionary.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Dictionary.h"
+#include "Options.h"
 #include <fstream>
 #include <iostream>
 
@@ -26,11 +27,18 @@ void Dictionary::create() {
 
 void Dictionary::addText(const std::filesystem::path &filename) {
     std::ifstream file(filename);
-    std::string prefix = "$ $"; //two nonwords are used
+    uint32_t adjustSize = Options::instance()->prefixSize()*2 - 1;
+    std::string prefix(adjustSize, ' ');
+    for (uint32_t i = 0; i < adjustSize; i += 2){
+        prefix[i] = '$'; //preSize nonwords are used
+    }
     std::string suffix;
     while (file >> suffix){
-
+        dictionary[prefix].push_back(suffix);
+        prefix = prefix.substr(prefix.find(' ') + 1) + ' ' + suffix;
+        //looks for space, reappends with space
     }
-
+    //need to fix last prefix with nonword
+    dictionary[prefix].push_back("$");
 }
 
